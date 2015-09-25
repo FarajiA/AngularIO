ba785bb48da1ad9a6e13d5c1be18e4efce4d0b3b
FarajiA <sacbeme04@yahoo.com>
2015-09-25 17:32:08 -0500

    map stuff v1.4

--- www/components/dash/dash.js
+++ www/components/dash/dash.js
@@ -1,10 +1,12 @@
 ﻿; (function () {
     var app = angular.module('App');
     app.controller('DashController', ['$scope', 'UserObject', function ($scope, UserObject) {
-        // reusable authorization
-       
-        console.log("Enter dashboard controller");
+
         $scope.user = UserObject.data();
+
+        if ($scope.user.broadcasting) {
+
+        }
 
     }]);
 })();
--- www/components/dash/dashServices.js
+++ www/components/dash/dashServices.js
@@ -1,1 +1,26 @@
-﻿
+﻿; (function () {
+    angular.module('App').factory('Dash', ['$http', '$q', 'UserObject', function ($http, $q, UserObject) {
+        var data = [];
+        var User = {};
+
+        User.broadcast = function () {
+            var deffered = $q.defer();
+            var guid = UserObject.data().GUID;      
+            var msg = { "guid": guid, "password": password };
+
+            $http.put(baseURL + "api/users/broadcast/")
+            .success(function (d) {
+                data = d;
+                deffered.resolve();
+            })
+            .error(function (data, status) {
+                console.log("Request failed " + status);
+            });
+            return deffered.promise;
+        };
+
+        User.data = function () { return data };
+        return User;
+    }]);
+
+})();
