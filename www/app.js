45385dbc24f84e985bd90528a16dc0cc854b197b
FarajiA <sacbeme04@yahoo.com>
2016-01-03 18:27:57 -0600

    Update background location msg

--- www/app.js
+++ www/app.js
@@ -815,10 +815,10 @@ app.controller('initController', ['$scope', '$timeout', '$interval', '$window', 
                 desiredAccuracy: 10,
                 stationaryRadius: 20,
                 distanceFilter: 30,
-                debug: true, // <-- enable this hear sounds for background-geolocation life-cycle. 
+                //debug: true, // <-- enable this hear sounds for background-geolocation life-cycle. 
                 stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates 
                 notificationTitle: "Chaser",
-                notificationText: "doing stuff and stuff",
+                notificationText: "Broadcasting location...",
                 locationService: backgroundGeoLocation.service.ANDROID_DISTANCE_FILTER,
             });
         }
@@ -828,7 +828,7 @@ app.controller('initController', ['$scope', '$timeout', '$interval', '$window', 
                 stationaryRadius: 20,
                 distanceFilter: 30,
                 activityType: 'AutomotiveNavigation',
-                debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+                //debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
                 stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
             });
         }
