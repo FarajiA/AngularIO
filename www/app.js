66c941dbb41c5ad08ba8319eaafa250f99da431a
FarajiA <sacbeme04@yahoo.com>
2016-03-31 23:09:40 -0500

    Contacts Android fix

--- www/app.js
+++ www/app.js
@@ -948,10 +948,8 @@ app.controller('initController', ['$scope', '$timeout', '$interval', '$window', 
             for (var i = 0; i < allContacts.length; i++) {
                 if (allContacts[i].phoneNumbers != null && allContacts[i].phoneNumbers[0].type === 'mobile') {
                     var contactphone = allContacts[i].phoneNumbers;
-                    var phonenumber = _.pluck(_.where(contactphone, {'type': 'mobile'}), 'value');
+                    var phonenumber = contactphone[0].value;
                     var name = allContacts[i].displayName;
-
-                    var name = allContacts[i].name.formatted;
                       var contact = {
                                name: '',
                                phonenumber: ''
